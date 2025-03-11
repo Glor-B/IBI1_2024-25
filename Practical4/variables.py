c6bc386e7f8d3f98342643047297a1b3c77017e6
Gloria <zihan1.24@intl.zju.edu.cn>
2025-03-11 10:05:41 +0800

    finish variables.py

---
+++ Practical4/variables.py
@@ -0,0 +1,18 @@
+# a is time for woal to the bus stop
+a = 15
+# b is time for bus journey
+b = 75
+# c is the total time for walking to the office
+c = a + b
+# d is time for driving to office
+d = 90
+# e is time for walking to car park
+e = 5
+# f is the total time for driving to the office
+f = d + e
+# c < f, so walking to office is slightly faster.
+
+X = True
+Y = False
+W = X and Y
+# W is False
