b762761f75c28079632b7927f33f3b5ef46a07c3
Gloria <zihan1.24@intl.zju.edu.cn>
2025-03-11 10:40:21 +0800

    finish the bmi calculator

---
+++ Practical4/bmi_calculator.py
@@ -0,0 +1,14 @@
+# store one's weight and height
+# a: weight; b: height
+a = 60
+b = 1.70
+# calculate one's bmi
+bmi = a / b ** 2
+# judge the bmi range
+if bmi <= 18.5:
+    # print the result
+    print("Your BMI is", bmi, "You are underweight! Please eat more :(")
+elif bmi >= 30:
+    print("Your BMI is bmi", bmi, "Sorry, you are obese! Wanna some exercise?")
+else:
+    print("Your BMI is", bmi,". Wow！You are in normal weight!")
