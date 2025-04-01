9c76f7e30f49c3c8ab6ec6c9964dc748eed82842
Gloria <zihan1.24@intl.zju.edu.cn>
2025-04-01 10:27:21 +0800

    add length counter

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -2,4 +2,7 @@ import re
 
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 largest_intron = re.findall(r'GT[a-zA-Z]+AG', seq)
-print(largest_intron)
+counter = re.split(r'.', largest_intron[0])
+#print(counter)
+length = len(counter) - 1
+print(largest_intron, length)
