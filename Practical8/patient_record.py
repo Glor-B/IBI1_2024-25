44d0bda83ee21ccb7935193c92ddc13b9e13c716
Gloria <zihan1.24@intl.zju.edu.cn>
2025-04-09 12:44:31 +0800

    change output to 1 line

--- Practical8/patient_record.py
+++ Practical8/patient_record.py
@@ -1,11 +1,11 @@
-class patient_record():
+class patient_record:
     def __init__(self, name, age, latest_submission, medical_history):
         self.name = name
         self.age = age
         self.latest_submission = latest_submission
         self.medical_history = medical_history
     def record(self):
-        return 'Patient name:'+ self.name + '\n' + "Patient age:" + self.age + '\n' + "Update date:" + self.latest_submission + '\n' + "Medical history:" + self.medical_history
+        return 'Patient name:'+ self.name + '\t' + "Patient age:" + self.age + '\t' + "Update date:" + self.latest_submission + '\t' + "Medical history:" + self.medical_history
 
 name, age, latest_submission, medical_history = input('Enter patient name:'), input('Enter patient age:'), input('Enter last usbmission data:'), input('Enter patient medical history:')
 
