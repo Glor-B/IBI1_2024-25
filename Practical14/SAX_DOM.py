e5cfa00b68ed95f4cd7822189a154e6133d1d71e
Gloria <zihan1.24@intl.zju.edu.cn>
2025-05-21 16:05:24 +0800

    multiple output if same number of is_a

--- Practical14/SAX_DOM.py
+++ Practical14/SAX_DOM.py
@@ -12,44 +12,46 @@ term = collection.getElementsByTagName('term')
 
 # dataframe to store isamax and according terms for each namespace
 max_dom = pd.DataFrame(
-    columns = ['Namespace', 'id', 'is_a']
+    columns = ['Namespace', 'name', 'is_a']
 )
 max_dom.set_index('Namespace', inplace = True)
 
 for terms in term:
     namespace = terms.getElementsByTagName('namespace')[0].firstChild.nodeValue
-    id = terms.getElementsByTagName('id')[0].firstChild.nodeValue
-    """ defs = terms.getElementsByTagName('def')
-    defstr = defs.getElementsByTagName('defstr') """
+    name = terms.getElementsByTagName('name')[0].firstChild.nodeValue
     is_a = len(terms.getElementsByTagName('is_a'))
-    # print(type(is_a))
-    # print(namespace, id, is_a)
-    # print(max.loc[max['Namespace'] == namespace,'is_a'].iloc[0])
-    # print(max.loc[max['Namespace'] == namespace,'is_a'].iloc[0] < is_a)
     if namespace not in max_dom.index:
-        max_dom.loc[namespace,'is_a'] = is_a
-        max_dom.loc[namespace,'id'] = id
+        max_dom.loc[namespace,'is_a'] = [is_a]
+        max_dom.loc[namespace,'name'] = [name]
         # print(max)
     else:
-        if max_dom.loc[namespace,'is_a']< is_a:
-            max_dom.loc[namespace,'is_a'] = is_a
-            max_dom.loc[namespace,'id'] = id
+        if max_dom.loc[namespace,'is_a'][0] < is_a:
+            max_dom.loc[namespace,'is_a'] = [is_a]
+            max_dom.loc[namespace,'name'] = [name]
+        elif max_dom.loc[namespace, 'is_a'][0] == is_a:
+            max_dom.loc[namespace,'is_a'].append(is_a)
+            max_dom.loc[namespace,'name'].append(name)
 
 end_dom = datetime.now()
 difference_dom = end_dom - start_dom
 print("DOM processing:")
-print('[Biological process] GO id:', max_dom.loc['biological_process', 'id'], 'is_a number:', max_dom.loc['biological_process', 'is_a'])
-print('[Molecular function] GO id:', max_dom.loc['molecular_function', 'id'], 'is_a number:', max_dom.loc['molecular_function', 'is_a'])
-print('[Cellular component] GO id:', max_dom.loc['cellular_component', 'id'], 'is_a number:', max_dom.loc['cellular_component', 'is_a'])
-print('Time of processing:', difference_dom)
+print('[Biological process]')
+for i in range(len(max_dom.loc['biological_process','is_a'])):
+    print('Name:', max_dom.loc['biological_process', 'name'][i], 'is_a number',max_dom.loc['biological_process', 'is_a'][i])
+print('[Molecular function]')
+for i in range(len(max_dom.loc['molecular_function','is_a'])):
+    print('Name:', max_dom.loc['molecular_function', 'name'][i], 'is_a number',max_dom.loc['molecular_function', 'is_a'][i])
+print('[Cellular component]')
+for i in range(len(max_dom.loc['cellular_component','is_a'])):
+    print('Name:', max_dom.loc['cellular_component', 'name'][i], 'is_a number',max_dom.loc['cellular_component', 'is_a'][i])
+print('Time of processing:', difference_dom, '\n')
 
 start_sax = datetime.now()
 parser = xml.sax.make_parser()
-#parser.setFeature(xml.sax.handlerfeature_namespace,0)
 
 # dataframe to store isamax and according terms for each namespace
 max_sax = pd.DataFrame(
-    columns = ['Namespace', 'id', 'is_a']
+    columns = ['Namespace', 'name', 'is_a']
 )
 max_sax.set_index('Namespace', inplace = True)
 
@@ -57,7 +59,7 @@ class TermHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.currentdata = ''
         self.namespace = ''
-        self.id = ''
+        self.name = ''
         self.is_a = 0
     
     def startElement(self, tag, attributes):
@@ -67,8 +69,8 @@ class TermHandler(xml.sax.ContentHandler):
         if self.currentdata == 'namespace':
             self.namespace += content
             #print(self.namespace)
-        elif self.currentdata == 'id':
-            self.id = content
+        elif self.currentdata == 'name':
+            self.name += content
         elif self.currentdata == 'is_a':
             self.is_a += 1
     
@@ -77,16 +79,20 @@ class TermHandler(xml.sax.ContentHandler):
         if tag == 'term':
             # print(self.namespace not in max.index)
             if self.namespace not in max_sax.index:
-                max_sax.loc[self.namespace,'is_a'] = self.is_a
-                max_sax.loc[self.namespace,'id'] = self.id
+                max_sax.loc[self.namespace,'is_a'] = [self.is_a]
+                max_sax.loc[self.namespace,'name'] = [self.name]
         # print(max)
             else:
-                if max_sax.loc[self.namespace,'is_a'] < self.is_a:
-                    max_sax.loc[self.namespace,'is_a'] = self.is_a
-                    max_sax.loc[self.namespace,'id'] = self.id
+                if max_sax.loc[self.namespace,'is_a'][0] < self.is_a:
+                    max_sax.loc[self.namespace,'is_a'] = [self.is_a]
+                    max_sax.loc[self.namespace,'name'] = [self.name]
                     # print(max)
+                elif max_sax.loc[self.namespace, 'is_a'][0] == self.is_a:
+                    max_sax.loc[self.namespace,'is_a'].append(self.is_a)
+                    max_sax.loc[self.namespace,'name'].append(self.name)
             self.is_a = 0
             self.namespace = ''
+            self.name = ''
         self.currentdata = ''
 
 Handler = TermHandler()
@@ -96,7 +102,15 @@ parser.parse('go_obo.xml')
 end_sax = datetime.now()
 difference_sax = end_sax - start_sax
 print("SAX processing:")
-print('[Biological process] GO id:', max_sax.loc['biological_process', 'id'], 'is_a number:', max_sax.loc['biological_process', 'is_a'])
-print('[Molecular function] GO id:', max_sax.loc['molecular_function', 'id'], 'is_a number:', max_sax.loc['molecular_function', 'is_a'])
-print('[Cellular component] GO id:', max_sax.loc['cellular_component', 'id'], 'is_a number:', max_sax.loc['cellular_component', 'is_a'])
+print('[Biological process]')
+for i in range(len(max_sax.loc['biological_process','is_a'])):
+    print('Name:', max_sax.loc['biological_process', 'name'][i], 'is_a number',max_sax.loc['biological_process', 'is_a'][i])
+print('[Molecular function]')
+for i in range(len(max_sax.loc['molecular_function','is_a'])):
+    print('Name:', max_sax.loc['molecular_function', 'name'][i], 'is_a number',max_sax.loc['molecular_function', 'is_a'][i])
+print('[Cellular component]')
+for i in range(len(max_sax.loc['cellular_component','is_a'])):
+    print('Name:', max_sax.loc['cellular_component', 'name'][i], 'is_a number',max_sax.loc['cellular_component', 'is_a'][i])
 print('Time of processing:', difference_sax)
+
+# SAX is quicker that DOM
