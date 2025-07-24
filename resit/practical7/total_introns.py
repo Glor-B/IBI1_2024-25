13ff602a5819dab2906becde760b65b35195698c
Gloria <zihan1.24@intl.zju.edu.cn>
2025-07-24 13:10:55 +0800

    polish

--- resit/practical7/total_introns.py
+++ resit/practical7/total_introns.py
@@ -2,4 +2,4 @@ import re
 
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 total_intron = re.findall(r'(?=(GT.*AG))', seq) + re.findall(r'(?=(GT.*?AG))', seq)
-print(total_intron)
+print(len(total_intron))
